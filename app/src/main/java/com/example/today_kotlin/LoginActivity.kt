f112dc205bffdf874ab69a9f293e3f2a56e3eb5a
2tle <iam@2tle.io>
2021-05-16 00:27:33 +0900

    login

--- app/src/main/java/com/example/today_kotlin/LoginActivity.kt
+++ app/src/main/java/com/example/today_kotlin/LoginActivity.kt
@@ -2,10 +2,55 @@ package com.example.today_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity: AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    var TAG: String = "test>"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+        auth = Firebase.auth
+        var loginBtn = findViewById(R.id.loginBtn) as Button
+        var email = findViewById(R.id.email) as EditText
+        var pw = findViewById(R.id.pw) as EditText
+        loginBtn.setOnClickListener {
+            signIn(email.text.toString(),pw.text.toString())
+        }
+
     }
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if(currentUser != null) { /* reload(); */ }
+    }
+
+    private fun signIn(email: String, password: String) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        Toast.makeText(baseContext, "Authentication succeeded",
+                            Toast.LENGTH_SHORT).show()
+                        val user = auth.currentUser
+                        //updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                        //updateUI(null)
+                    }
+                }
+        // [END sign_in_with_email]
+    }
+
 }
