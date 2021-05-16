e080671d6f30e6b54d840af3302dad7ad4aaa386
2tle <iam@2tle.io>
2021-05-16 14:56:39 +0900

    register

---
+++ app/src/main/java/com/example/today_kotlin/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.example.today_kotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+    }
+}
