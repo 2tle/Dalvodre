5d0c1a2ffc5cf20ed09991852fdf420c0fb4c47f
2tle <iam@2tle.io>
2021-05-16 16:56:24 +0900

    register create

--- app/src/main/java/com/example/today_kotlin/LoginActivity.kt
+++ app/src/main/java/com/example/today_kotlin/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.today_kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -12,16 +13,20 @@ import com.google.firebase.ktx.Firebase
 
 class LoginActivity: AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
-    var TAG: String = "test>"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
         auth = Firebase.auth
         var loginBtn = findViewById(R.id.loginBtn) as Button
+        var regBtn = findViewById(R.id.register) as Button
         var email = findViewById(R.id.email) as EditText
         var pw = findViewById(R.id.pw) as EditText
         loginBtn.setOnClickListener {
             signIn(email.text.toString(),pw.text.toString())
+        }
+        regBtn.setOnClickListener {
+            val nextIntent = Intent(this, RegisterActivity::class.java)
+            startActivity(nextIntent)
         }
 
     }
@@ -32,25 +37,17 @@ class LoginActivity: AppCompatActivity() {
     }
 
     private fun signIn(email: String, password: String) {
-        // [START sign_in_with_email]
         auth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "signInWithEmail:success")
-                        Toast.makeText(baseContext, "Authentication succeeded",
+                        Toast.makeText(baseContext, "로그인 성공",
                             Toast.LENGTH_SHORT).show()
                         val user = auth.currentUser
-                        //updateUI(user)
                     } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "signInWithEmail:failure", task.exception)
-                        Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.makeText(baseContext, "로그인 실패, 다시 시도해주세요",
                                 Toast.LENGTH_SHORT).show()
-                        //updateUI(null)
                     }
                 }
-        // [END sign_in_with_email]
     }
 
 }
--- app/src/main/java/com/example/today_kotlin/RegisterActivity.kt
+++ app/src/main/java/com/example/today_kotlin/RegisterActivity.kt
@@ -3,11 +3,79 @@ package com.example.today_kotlin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
+    private var auth: FirebaseAuth = Firebase.auth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        val user = Firebase.auth.currentUser
+        if(user != null) {
+            Toast.makeText(baseContext, "로그인이 이미 되어있습니다.", //메인으로 넘어가기
+                Toast.LENGTH_SHORT).show()
+        }
+        var email = findViewById(R.id.email) as EditText
+        var pw = findViewById(R.id.pw) as EditText
+        var name = findViewById(R.id.username) as EditText
+        var regBtn = findViewById(R.id.regBtn) as Button
+
+        regBtn.setOnClickListener {
+            Toast.makeText(baseContext, "1111",
+                Toast.LENGTH_SHORT).show()
+            createAccount(email.text.toString(),pw.text.toString(), name.text.toString())
+            //writeNewUserWithTaskListeners()
+
+        }
+
 
     }
+
+    private fun createAccount(email: String, password: String, name: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(baseContext, "11",
+                        Toast.LENGTH_SHORT).show()
+                    val user = auth.currentUser
+                    createFireStoreUsername(user.uid,name,user.email)
+
+                } else {
+                    Toast.makeText(baseContext, "계정생성 실패, 다시 시도해주세요.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+
+    fun createFireStoreUsername(userId: String,name: String,email: String) {
+        val db = Firebase.firestore
+        val user = hashMapOf(
+            "userId" to userId,
+            "email" to email,
+            "name" to name
+        )
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Toast.makeText(baseContext, "계정생성 성공",
+                    Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(baseContext, "계정생성 실패, 다시 시도해주세요.",
+                    Toast.LENGTH_SHORT).show()
+            }
+    }
+
+
+
+
+
+
 }
