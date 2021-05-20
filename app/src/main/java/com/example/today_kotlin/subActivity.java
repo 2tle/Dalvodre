88f2dbc1641823710fe2b4617490023515f86d48
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-05-21 00:47:34 +0900

    오늘의 한마디 확인창 그저 넘어가는 창

---
+++ app/src/main/java/com/example/today_kotlin/subActivity.java
@@ -0,0 +1,14 @@
+package com.example.today_kotlin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class subActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+    }
+}
