423a05962a366e08aa8377153ccc5ad0be8182ab
2tle_ <sw4019@naver.com>
2021-05-22 01:22:59 +0900

    loading delay

--- app/src/main/java/com/example/today_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/today_kotlin/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.today_kotlin
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT:Long = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val nextIntent = Intent(this, LoginActivity::class.java)
-        startActivity(nextIntent)
+        val handler = Handler(Looper.myLooper()!!)
+        handler.postDelayed({
+            val intent = Intent(baseContext, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
+
 }
