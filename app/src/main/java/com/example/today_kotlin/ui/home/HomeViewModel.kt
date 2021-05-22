7182e108580f6eaa3b2b5b2da3a0b10c6365e333
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-05-22 23:14:36 +0900

    로그인 창

--- app/src/main/java/com/example/today_kotlin/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/today_kotlin/ui/home/HomeViewModel.kt
@@ -3,11 +3,11 @@ package com.example.today_kotlin.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-//함수 만들고 함수값을 아래 value에 넣으면 될듯
+//변수 만들고 변수(오늘의 한마디) 값을 아래 value에 넣으면 될듯
 class HomeViewModel : ViewModel() {
-
+    var word = "오늘의 한마디"
     private val _text = MutableLiveData<String>().apply {
-        value = "여기는 오늘의 한마디 들어갈거"
+        value = "\"\n"+word+"\n\""
     }
     val text: LiveData<String> = _text
 }
