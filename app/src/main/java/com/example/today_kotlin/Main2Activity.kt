33422aa4929999e6745e1a1b98a3cef5dde5ed97
양현준 <iam@2tle.io>
2021-05-25 21:59:20 +0900

    dbdbdb

--- app/src/main/java/com/example/today_kotlin/Main2Activity.kt
+++ app/src/main/java/com/example/today_kotlin/Main2Activity.kt
@@ -24,10 +24,11 @@ import com.bumptech.glide.Glide
 import com.google.api.Distribution
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
-import com.google.firebase.storage.ktx.storage
+import com.google.firestore.v1.FirestoreGrpc
 import com.example.today_kotlin.NavHeaderActivity as NavHeaderActivity
 
 class Main2Activity : AppCompatActivity() {
@@ -35,6 +36,7 @@ class Main2Activity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var auth: FirebaseAuth
     private lateinit var storage: FirebaseStorage
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,15 +53,17 @@ class Main2Activity : AppCompatActivity() {
                 R.id.nav_home, R.id.nav_save, R.id.nav_community
             ), drawerLayout
         )
+        var db = Firebase.firestore
 
-        var user = Firebase.auth.currentUser
+
+        val user = Firebase.auth.currentUser
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-        var navigationView: NavigationView = findViewById(R.id.nav_view)
-        var headerView: View = navigationView.getHeaderView(0)
-        var headerIcon: ImageView = headerView.findViewById(R.id.imageView)
-        var headerUsername: Button = headerView.findViewById(R.id.name_btn)
-        var headerEmail: TextView = headerView.findViewById(R.id.textView)
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        val headerView: View = navigationView.getHeaderView(0)
+        val headerIcon: ImageView = headerView.findViewById(R.id.imageView)
+        val headerUsername: Button = headerView.findViewById(R.id.name_btn)
+        val headerEmail: TextView = headerView.findViewById(R.id.textView)
         headerUsername.setOnClickListener {
             startActivity(Intent(this, settingActivity::class.java))
         }
