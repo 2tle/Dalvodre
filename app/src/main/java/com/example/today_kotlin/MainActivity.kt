c5fc511eecd7d4d82d77368f6f62acc27c898236
JiHyun13 <83816873+JiHyun13@users.noreply.github.com>
2021-05-25 23:26:15 +0900

    시간에 따라 테마 변경(main, main2, write Activity 변경)

--- app/src/main/java/com/example/today_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/today_kotlin/MainActivity.kt
@@ -4,7 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 
 class MainActivity : AppCompatActivity() {
     private val SPLASH_TIME_OUT:Long = 3000
@@ -13,11 +18,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val handler = Handler(Looper.myLooper()!!)
+        val dateAndtime: LocalDateTime = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("HH")
+        var formatted = dateAndtime.format(formatter)
+        var background = findViewById<androidx.constraintlayout.widget.ConstraintLayout>(R.id.background)
+
+        if(formatted.toInt()>=5 && formatted.toInt()<=18)
+            background.setBackgroundResource(R.drawable.not)
+        else if(formatted.toInt()>=16 && formatted.toInt()<=21)
+            background.setBackgroundResource(R.drawable.dinner)
+
         handler.postDelayed({
             val intent = Intent(baseContext, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }, 3000)
-    }
 
+    }
 }
--- app/src/main/java/com/example/today_kotlin/writeActivity.kt
+++ app/src/main/java/com/example/today_kotlin/writeActivity.kt
@@ -2,10 +2,23 @@ package com.example.today_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class writeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write)
+
+        val dateAndtime: LocalDateTime = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("HH")
+        var formatted = dateAndtime.format(formatter)
+        var background = findViewById<TextView>(R.id.write_word)
+
+        if(formatted.toInt()>=5 && formatted.toInt()<=18)
+            background.setBackgroundResource(R.drawable.not)
+        else if(formatted.toInt()>=16 && formatted.toInt()<=21)
+            background.setBackgroundResource(R.drawable.dinner)
     }
 }
